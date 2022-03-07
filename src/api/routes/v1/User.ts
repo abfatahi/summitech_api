c71f879b55173eb82281f19163b1e86ef2acfd8f
Ishaq Abdulfatahi <abfatahi.iaf@gmail.com>
2022-03-07 10:24:02 +0100

    fix: updates

--- src/api/routes/v1/User.ts
+++ src/api/routes/v1/User.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { body, header } from 'express-validator';
+import { body, header, param } from 'express-validator';
 import { UserMiddleware } from '../../middlewares';
 import Controller from '../../controllers/User';
 
@@ -8,27 +8,42 @@ const UserController = Controller();
 const router = express.Router();
 
 router.post(
-    '/fund-deposit',
-    [
-      header(
-        'Authorization',
-        'Unauthorized! Sign in to your account for authorization'
-      )
-        .exists()
-        .bail()
-        .custom((value: any) => UserMiddleware.isValidUserToken(value)),
-      body('amount', 'Failed! Amount cannot be blank')
-        .exists()
-        .bail()
-        .isNumeric()
-        .withMessage('Failed! Fullname must be a numeric value')
-        .trim(),
-      body('beneficiary', 'Failed! Beneficairy cant be blank')
-        .exists()
-        .custom((beneficiary) => UserMiddleware.isUserExist(beneficiary)),
-    ],
-    UserController.funds_transfer
-  );
+  '/fund-wallet',
+  [
+    header(
+      'Authorization',
+      'Unauthorized! Sign in to your account for authorization'
+    )
+      .exists()
+      .bail()
+      .custom((value: any) => UserMiddleware.isValidUserToken(value)),
+    body('amount', 'Failed! Amount cannot be blank')
+      .exists()
+      .bail()
+      .isNumeric()
+      .withMessage('Failed! Fullname must be a numeric value')
+      .trim(),
+    body('beneficiary', 'Failed! Beneficairy cant be blank')
+      .exists()
+      .custom((beneficiary) => UserMiddleware.isUserExist(beneficiary)),
+  ],
+  UserController.funds_transfer
+);
+
+router.get(
+  '/fund-wallet/verify/:reference',
+  [
+    header(
+      'Authorization',
+      'Unauthorized! Sign in to your account for authorization'
+    )
+      .exists()
+      .bail()
+      .custom((value: any) => UserMiddleware.isValidUserToken(value)),
+    param('reference', 'Failed! Reference cannot be blank').exists().bail(),
+  ],
+  UserController.verify_fund_wallet
+);
 
 router.post(
   '/transfer',
